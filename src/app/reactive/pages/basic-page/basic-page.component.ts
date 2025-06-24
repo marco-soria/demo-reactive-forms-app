32e79107dbe0374e783f964c4b3f3ef0b42d2d3f
marco-soria <marco.soriaa7@gmail.com>
2025-06-24 09:34:53 -0500

    initial folder config

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [],
+  templateUrl: './basic-page.component.html',
+  styleUrl: './basic-page.component.css'
+})
+export class BasicPageComponent {
+
+}
